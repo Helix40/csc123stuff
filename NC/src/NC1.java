59a57010aaa6a5a3cf539fc4ade348f0da4bdd66
omega <omega@10.164.27.85>
2023-09-09 14:01:23 -0700

    Hello

---
+++ NC/src/NC1.java
@@ -0,0 +1,25 @@
+
+public class NC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+		byte b = (byte) a;
+		System.out.println (b);
+		
+		byte s1=20;
+		
+		short b1= (byte)s1;
+		
+		System.out.println (b1);
+		
+		// Casting 2
+		
+		int i =65;
+		char c=(char)i;
+		System.out.println(c); 
+		
+
+	}
+
+}
